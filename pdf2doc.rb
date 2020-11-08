4dbfea61dc6b0f3397c7a8081d59dc1b13ebbe2d
Ryuhey_iMac <ryuhey0123@gmail.com>
2020-11-08 16:13:22 +0900

    Add formula

---
+++ pdf2doc.rb
@@ -0,0 +1,14 @@
+class Pdf2doc < Formula
+  desc "Add page number to some PDF files and merge it → PERFECT DOCUMENT! ⭐️"
+  homepage "https://github.com/ryuhey0123/pdf2doc"
+  url "https://github.com/ryuhey0123/pdf2doc/releases/download/v1.0/release-v1.0.tar.gz"
+  sha256 "041c93e73caf9a0bdcf4308b0789a12870bf1c1312f84a106a6fa50916a6a708"
+  license "MIT"
+
+  def install
+    bin.install 'pdf2doc'
+
+  end
+
+end
+
