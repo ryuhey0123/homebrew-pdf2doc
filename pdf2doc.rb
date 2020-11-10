9f08af6fb6a478a7507a4c6835b947bea153f4d0
ryuhey_biz <none>
2020-11-10 18:41:42 +0900

    Pyinstaller delete

--- pdf2doc.rb
+++ pdf2doc.rb
@@ -5,7 +5,7 @@ class Pdf2doc < Formula
   desc "Add page number to some PDF files and merge it → PERFECT DOCUMENT! ⭐️"
   homepage "https://github.com/ryuhey0123/pdf2doc"
   # url "https://github.com/ryuhey0123/pdf2doc/releases/download/v1.0/release-v1.0.tar.gz"
-  url "https://github.com/ryuhey0123/pdf2doc.git"
+  url "https://github.com/ryuhey0123/pdf2doc.git -b v1.0"
   # sha256 "041c93e73caf9a0bdcf4308b0789a12870bf1c1312f84a106a6fa50916a6a708"
   license "MIT"
 
