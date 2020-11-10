1f9237c2d0de8c84816196218fd127ddd4be6f1a
ryuhey_biz <none>
2020-11-10 21:54:13 +0900

    三度目の正直

--- pdf2doc.rb
+++ pdf2doc.rb
@@ -1,8 +1,8 @@
 class Pdf2doc < Formula
   desc "Add page number to some PDF files and merge it → PERFECT DOCUMENT! ⭐️"
   homepage "https://github.com/ryuhey0123/pdf2doc"
-  url "https://github.com/ryuhey0123/pdf2doc/releases/download/v1.0/release-v1.0.0.tar.gz"
-  sha256 "550d89b49a0edaf2136281c8f92019f0a4b6809417076d9077f7a26cf757db17"
+  url "https://github.com/ryuhey0123/pdf2doc/releases/download/v1.0.0/release-v1.0.tar.gz"
+  sha256 "041c93e73caf9a0bdcf4308b0789a12870bf1c1312f84a106a6fa50916a6a708"
   license "MIT"
 
   def install
